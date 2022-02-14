1ad9ea0638b1e7106ed8a4e41d1dd37d186d1820
flowingwings <38078537+flowingwings@users.noreply.github.com>
2022-02-14 21:17:19 +0800

    127 is NOT solved yet.

---
+++ 127. Word Ladder/Solve.java
@@ -0,0 +1,14 @@
+import java.util.List;
+
+class Solve{
+    public static void main(String[] args) {
+        return;
+    }
+}
+
+class Solution1{
+    public int ladderLength(String beginWord, String endWord, List<String> wordList){
+        // TODO
+        return 1;
+    }
+}
