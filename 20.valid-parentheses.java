bc3934527f5e4ca50a423126d58d5f5f559e5173
flowingwings <wangyinbo1999@gmail.com>
2022-02-26 04:09:29 +0800

    Solved 20.

---
+++ 20.valid-parentheses.java
@@ -0,0 +1,39 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+/*
+ * @lc app=leetcode id=20 lang=java
+ *
+ * [20] Valid Parentheses
+ */
+
+// @lc code=start
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        for(int i=0; i<s.length(); i++){
+            char c = s.charAt(i);
+            if(c == '(' || c == '[' || c == '{'){
+                st.push(new Character(c));
+            }
+            else{
+                if(st.empty()){
+                    return false;
+                }
+                if((c == ')' && st.peek() == '(') || (c==']' && st.peek()=='[') || (c=='}' && st.peek()=='{')){
+                    st.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        if(!st.empty()){
+            return false;
+        }
+        return true;
+    }
+}
+// @lc code=end
+
