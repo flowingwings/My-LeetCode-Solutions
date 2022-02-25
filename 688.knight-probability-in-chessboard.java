20c84c639dd393a3f5de64f31d80506f46f0fe7a
flowingwings <38078537+flowingwings@users.noreply.github.com>
2022-02-26 04:14:27 +0800

    688 NOT solved yet.

---
+++ 688.knight-probability-in-chessboard.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=688 lang=java
+ *
+ * [688] Knight Probability in Chessboard
+ */
+
+// @lc code=start
+class Solution {
+    public double knightProbability(int n, int k, int row, int column) {
+        
+    }
+}
+// @lc code=end
+
