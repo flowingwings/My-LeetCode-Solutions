b32c4bccaf2c60d1f7ab2d3fe77e69723f9839ba
flowingwings <wangyinbo1999@gmail.com>
2022-03-01 00:53:12 +0800

    CVed 319.

---
+++ 319.bulb-switcher.java
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode id=319 lang=java
+ *
+ */
+
+// @lc code=start
+
+//Copied from https://leetcode.com/problems/bulb-switcher/discuss/77104/Math-solution..
+//A totally genius thought...
+class Solution {
+    public int bulbSwitch(int n) {
+        return (int) (Math.sqrt(n));
+    }
+}
+// @lc code=end
+
+
