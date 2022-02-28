d4344284811f840502c2a3898b905791cf5f7acf
flowingwings <wangyinbo1999@gmail.com>
2022-03-01 02:03:09 +0800

    Solved 226.

---
+++ 226.invert-binary-tree.java
@@ -0,0 +1,38 @@
+// /*
+//  * @lc app=leetcode id=226 lang=java
+//  *
+//  * [226] Invert Binary Tree
+//  */
+
+// // @lc code=start
+// public class TreeNode {
+//     int val;
+//     TreeNode left;
+//     TreeNode right;
+
+//     TreeNode() {
+//     }
+
+//     TreeNode(int val) {
+//         this.val = val;
+//     }
+
+//     TreeNode(int val, TreeNode left, TreeNode right) {
+//         this.val = val;
+//         this.left = left;
+//         this.right = right;
+//     }
+// }
+
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if(root==null){
+            return root;
+        }
+        TreeNode temp = root.left;
+        root.left=invertTree(root.right);
+        root.right=invertTree(temp);
+        return root;
+    }
+}
+// @lc code=end
