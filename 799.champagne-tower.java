3c54b06c882dadf46c3cf995a0f1d936e2100993
flowingwings <wangyinbo1999@gmail.com>
2022-03-04 23:45:26 +0800

    CVed 799.

---
+++ 799.champagne-tower.java
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=799 lang=java
+ *
+ * [799] Champagne Tower
+ */
+
+// @lc code=start
+class Solution {
+    public double champagneTower(int poured, int query_row, int query_glass) {
+        double[][] A = new double[102][102];
+        A[0][0] = (double) poured;
+        for(int r=0; r<query_row; r++){
+            for(int c=0; c<=r; c++){
+                double q=(A[r][c] - 1.0)/2.0;
+                if(q>0){
+                    A[r+1][c] += q;
+                    A[r+1][c+1] += q;
+                }
+            }
+        }
+
+        return Math.min(1, A[query_row][query_glass]);
+    }
+}
+// @lc code=end
+
