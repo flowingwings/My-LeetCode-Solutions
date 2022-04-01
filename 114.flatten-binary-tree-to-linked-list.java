eee79b924b70a6be8ca1d91391c3d4dec5e8a060
flowingwings <wangyinbo1999@gmail.com>
2022-04-01 14:18:55 +0800

    Solved 114.

---
+++ 114.flatten-binary-tree-to-linked-list.java
@@ -0,0 +1,55 @@
+/*
+ * @lc app=leetcode id=114 lang=java
+ *
+ * [114] Flatten Binary Tree to Linked List
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+class Solution {
+    public void flatten(TreeNode root) {
+        // link left sub tree, and link right sub tree.
+        if(root==null){
+            return;
+        }
+        TreeNode left = root.left;
+        TreeNode right = root.right;
+        flatten(left);
+        flatten(right);
+        root.left = null;
+        root.right = left;
+        while(root.right != null){
+            root = root.right;
+        }
+        root.right = right;
+        return;
+    }
+}
+// @lc code=end
+
